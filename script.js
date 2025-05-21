1eec3cb765e3c663cef7fac9fe733b23c67bc8ff
Dhanush Kunchakuri <132380514+dhanushkunchakuri@users.noreply.github.com>
2025-05-21 11:13:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let date=document.getElementById("timer");
+let curr=new Date();
+
+setInterval(
+	function(){
+		curr=new Date();
+		date.innerHTML=curr.toLocaleString();
+	},1000
+);
